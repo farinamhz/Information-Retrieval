75d62ecfe4925d396fa8ecc4445229f1ea5f4c6b
Farinam HemmatiZadeh <45711614+farinamhz@users.noreply.github.com>
2021-06-21 20:38:17 +0430

    IdMap finished

--- Indexing-BSBI/information_retrieval/helper.py
+++ Indexing-BSBI/information_retrieval/helper.py
@@ -12,7 +12,7 @@ class IdMap:
     def _get_str(self, i):
         """Returns the string corresponding to a given id (`i`)."""
         ### Begin your code
-
+        return self.id_to_str[i]
         ### End your code
 
     def _get_id(self, s):
@@ -20,7 +20,12 @@ class IdMap:
         If `s` is not in the IdMap yet, then assigns a new id and returns the new id.
         """
         ### Begin your code
-
+        if s not in self.str_to_id.keys():
+            id_new = len(self)
+            self.id_to_str.append(s)
+            self.str_to_id[s] = id_new
+            return id_new
+        return self.str_to_id[s]
         ### End your code
 
     def __getitem__(self, key):
