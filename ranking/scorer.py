5acf80420fc1007f046ec5b84609857429d45df9
Farinam HemmatiZadeh <45711614+farinamhz@users.noreply.github.com>
2021-06-28 22:53:47 +0430

    scorer updated (for last changes)

--- ranking/scorer.py
+++ ranking/scorer.py
@@ -1,5 +1,8 @@
 from ranking.idf import Idf
 from indexer_plus.constructor import BSBIIndex
+from indexer_plus.text_preprocess import TextCleaner
+from indexer_plus.utils import DocInfo
+# from indexer_plus.helper import IdMap
 
 
 class BM25Scorer:
@@ -12,6 +15,13 @@ class BM25Scorer:
         self.idf = idf
         self.index = index
 
+        self.b = 0.75
+        self.k1 = 1.5
+        self.N, self.avg_length = self.calc_avg_length()
+
+        self.text_cleaner = TextCleaner()
+        self.doc_info = DocInfo(index.term_id_map)
+
         self.default_query_weight_scheme = {"tf": 'b', "df": 't', "norm": None}  # boolean, idf, none
         self.default_doc_weight_scheme = {"tf": 'n', "df": 'n', "norm": None}  # natural, none
 
@@ -20,24 +30,51 @@ class BM25Scorer:
         self.doc_weight_scheme = doc_weight_scheme if doc_weight_scheme is not None \
             else self.default_doc_weight_scheme  # Modified (added)
 
-    def get_sim_score(self, q, d):
-        """ Score each document for each query.
-        Args:
-            q (Query): the Query
-            d (Document) :the Document
-
-        Returns:
-            pass now, will be implement in task 1, 2 and 3
-        """
-
     ### Begin your code
+    # def countfrequency(self, my_list):
+    #     # delete when they add tokenzier and tf calculator
+    #     freq = {}
+    #     for item in my_list:
+    #         if item in freq:
+    #             freq[item] += 1
+    #         else:
+    #             freq[item] = 1
+    #     return freq
+
+    def calc_avg_length(self):
+        # get from json files and calculate the avg
+        len_sum = 0
+        docinfo_len = len(dict(self.doc_info))
+        for i in range(docinfo_len):
+            len_sum += self.doc_info[i]['length']
+        avg = len_sum / docinfo_len
+        return docinfo_len, avg
+
+    def get_total_score(self, q, query_vec, d, doc_vec):
+
+        f = dict()
+        for t in query_vec.keys():
+            if t in doc_vec:
+                f[t] += doc_vec[t]
+        score = sum([query_vec[t] * ((f[t] * (self.k1 + 1))
+                                     / (f[t] + self.k1 *
+                                        (1 - self.b + self.b * (len(d) / self.avg_length))))
+                     for t in query_vec.keys()])
+        return score
 
     ### End your code
 
     def get_query_vector(self, q):
         query_vec = {}
         ### Begin your code
+        query_dic = {}
+        for term, tf in q.items():
+            query_dic[self.index.term_id_map[term]] = tf
 
+        for term, tf in query_dic.items():
+            term_idf = self.idf.get_idf(term)
+            # query_vec[term] = math.log(((self.N - term_idf + 0.5) / (term_idf + 0.5)) + 1)
+            query_vec[term] = term_idf
         ### End your code
         return query_vec
 
@@ -45,6 +82,15 @@ class BM25Scorer:
         doc_vec = {}
 
         ### Begin your code
+        doc_wordlist = d.split()
+        query_wordlist = q.split()
+        frequency_of_doc_dic = self.countfrequency(doc_wordlist)
+        for qw in query_wordlist:
+            if qw in frequency_of_doc_dic:
+                if qw not in doc_vec:
+                    doc_vec[qw] = frequency_of_doc_dic[qw]
+            else:
+                doc_vec[qw] = 0
 
         ### End your code
 
@@ -53,9 +99,15 @@ class BM25Scorer:
 
     def normalize_doc_vec(self, q, d, doc_vec):
         ### Begin your code
-
+        x = 0
+        for item in doc_vec:
+            x += (doc_vec[item] * doc_vec[item])
+        for item in doc_vec:
+            doc_vec[item] = doc_vec[item] / math.sqrt(x)
+        return doc_vec
         ### End your code
         ...
+
     def bm25f_normalize_doc_vec(self, q, d, doc_vec):
         """ Normalize the raw term frequencies in fields in document d
             using above equation (1).
@@ -70,18 +122,18 @@ class BM25Scorer:
 
         ### End your code
 
-    def calc_avg_length(self):
-        """ Set up average lengths for BM25F, also handling PageRank.
-        You need to
-        Initialize any data structures needed.
-        Perform any preprocessing you would like to do on the fields.
-        Handle pagerank
-        Accumulate lengths of fields in documents.
-        Hint: You could use query_dict
+    def get_sim_score(self, q, d):
+        """ Score each document for each query.
+        Args:
+            q (Query): the Query
+            d (Document) :the Document
+        Returns:
+            pass now, will be implement in task 1, 2 and 3
+            *** score ***
         """
-        ### Begin your code
-
-        ### End your code
+        query_vec = self.get_query_vector(q)
+        norm_doc_vec = self.get_doc_vector(q, d)
+        return self.get_total_score(q, query_vec, d, norm_doc_vec)
 
 
 if __name__ == '__main__':
