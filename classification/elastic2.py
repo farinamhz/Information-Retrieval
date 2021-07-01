c40d5f37981fbc126143dd3823bd5b9b3747eb42
zolfaShefreie <z.shefreie@gmail.com>
2021-07-01 12:56:36 +0430

    last changes for query elastic

--- classification/elastic2.py
+++ classification/elastic2.py
@@ -4,6 +4,7 @@ from typing import *
 
 from text_preprocess import TextCleaner
 
+
 class ElasticHandler:
 
     def __init__(self, index_name: str, els=Elasticsearch()):
@@ -36,11 +37,44 @@ class ElasticHandler:
         if with_category:
             fields.append('category')
         query = TextCleaner().get_clean_text(query)
-        return self.els.search(index=self.index_name,
-                               body={"query": {
-                                   "multi_match": {
-                                       "query": query,
-                                       "fields": fields}}})['hits']['hits']
+        converted_and_query = self.get_and_query(query)
+        converted_or_query = self.get_or_query(query)
+        should = [
+            {
+                "query_string": {
+                    "query": converted_and_query,
+                    "fields": ['text', ]}
+            }
+        ]
+        if with_category:
+            should.append({"query_string": {
+                "query": converted_or_query,
+                "fields": ['category', ]}})
+        query_body = {"query": {
+            "bool": {
+                "should": should
+            }}}
+
+        return self.els.search(index=self.index_name, body=query_body)['hits']['hits']
+
+    @staticmethod
+    def get_and_query(query):
+        query_words = query.split()
+        query = str()
+        for each in query_words[:len(query_words) - 1]:
+            query += f'({each}) AND '
+        query += f'({query_words[-1]})'
+        return query
+
+    @staticmethod
+    def get_or_query(query, with_star=True):
+        char = "*" if with_star else ""
+        query_words = query.split()
+        query = str()
+        for each in query_words[:len(query_words) - 1]:
+            query += f'({char}{each}{char})OR '
+        query += f'({char}{query_words[-1]}{char})'
+        return query
 
     def delete_all_docs(self):
         self.els.delete_by_query(index=[self.index_name, ], body={"query": {"match_all": {}}})
